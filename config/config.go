aee9e19fd65b786c56a80145e14b49189533694a
CrazyThursdayV50 <alex@nevercare.vip>
2024-09-11 17:41:38 +0800

    add config

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"errors"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// Parse config file
+func parseConfig[Config any](v *viper.Viper) (*Config, error) {
+	var c Config
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+// Load config file from given path
+func loadConfig(path, name string) (*viper.Viper, error) {
+	v := viper.New()
+	v.AddConfigPath(path)
+	v.SetConfigName(name)
+	v.SetConfigType("yml")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	return v, nil
+}
+
+// Get config
+func GetConfig[Config any](path, name string) (*Config, error) {
+	cfgFile, err := loadConfig(path, name)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := parseConfig[Config](cfgFile)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
